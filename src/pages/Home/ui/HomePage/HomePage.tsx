459ddb552b3b2b15188d4cb605c3ab9b01766ae2
anastasia-kashirova <kashirova.nastya@gmail.com>
2026-07-08 13:23:17 +0300

    fix

--- src/pages/Home/ui/HomePage/HomePage.tsx
+++ src/pages/Home/ui/HomePage/HomePage.tsx
@@ -7,7 +7,7 @@ import { HomeHero } from '../HomeHero/HomeHero';
 export const HomePage = () => {
   return (
     <Layout>
-      <Box as="main" color="#2d3c2d" minHeight="100%" overflow="hidden">
+      <Box as="main" color="#2d3c2d" minHeight="100%">
         <HomeHero />
         <HomeCategoriesSection />
       </Box>
